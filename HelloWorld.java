9a4728a225abb6731bebd32ef0946369208bbc6c
liuying <judgment0903@gmail.com>
2021-06-27 15:04:05 +0800

    3rd version

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		int[][] x = new int[3][3];
-		system.out.println("測試用");
+		System.out.println("測試用");
+		
 		for( int i = 0 ; i < 3 ; i++){
 			for( int j = 0 ; j < 3 ; j++){
 			
