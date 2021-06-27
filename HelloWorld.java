1f3a58090f3aed5fa4fe9c920eb571790d3968c9
liuying <judgment0903@gmail.com>
2021-06-27 14:14:03 +0800

    1st version

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package xxx;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		int[][] x = new int[3][3];
+		
+		for( int i = 0 ; i < 3 ; i++){
+			for( int j = 0 ; j < 3 ; j++){
+			
+			x[i][j] = (int)(Math.random() * 31);
+			System.out.print(x[i][j] + " ");
+
+			}
+			System.out.println();
+		}
+	}
+	
+}
